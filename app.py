adb4399956431f56237816a42d2b2b618e84195c
USAGI7878 <143148384+USAGI7878@users.noreply.github.com>
2025-05-05 22:25:56 +0800

    app.py

--- app.py
+++ app.py
@@ -257,7 +257,7 @@ cat_images = {
     "confused": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fmakerworld.com%2Fen%2Fmodels%2F921094-confused-cat-meme-cover-of-a1-a1mini-extruder&psig=AOvVaw3nlTarJCdIooX0UGKNezU4&ust=1746541033453000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCLD3ucLCjI0DFQAAAAAdAAAAABAE"  # 用实际的 URL 替换
 }
 
-st.image(cat_images[st.session_state.cat_mood], width=300, caption="Your cat's current mood 🐾"）
+st.image(cat_images[st.session_state.cat_mood], width=300, caption="Your cat's current mood 🐾")
 
 # 如果亲密值达到 10，显示猫咪主动过来的图
 if st.session_state.cat_coming:
